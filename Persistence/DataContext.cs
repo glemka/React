98fc7370893dc0cbf8ddd2f037d333baf78bf13c
glemka <42246414+glemka@users.noreply.github.com>
2019-12-30 22:08:31 +0100

    initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {            
+        }
+
+        public DbSet<Domain.Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+                builder.Entity<Domain.Value>()
+                .HasData(
+                    new Domain.Value{ Id = 1, Name = "Value 1"},
+                    new Domain.Value{ Id = 2, Name = "Value 2"},
+                    new Domain.Value{ Id = 3, Name = "Value 3"},
+                    new Domain.Value{ Id = 4, Name = "Value 4"}
+                );
+        }
+    }
+
+}
