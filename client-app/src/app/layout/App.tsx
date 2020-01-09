4c0b071e09410d002e886e9a1419813ac06ac7df
glemka <42246414+glemka@users.noreply.github.com>
2020-01-09 21:11:34 +0100

    Add router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,15 @@
-import React, { Fragment, useEffect, useContext } from "react";
+import React, {
+  Fragment,  
+  useEffect,
+  useContext
+} from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import LoadingComponent from "./LoadingComponent";
 import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
+import { Route } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 
@@ -22,7 +27,9 @@ const App = () => {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Route path='/' component={HomePage}/>
+        <Route path='/activities' component={ActivityDashboard}/>
+        <Route path='/createActivity' component={ActivityForm}/>
       </Container>
     </Fragment>
   );
