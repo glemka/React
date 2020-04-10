3e351ec5ab961afe96e59c08ad3e5bc2c5c5e096
glemka <42246414+glemka@users.noreply.github.com>
2020-04-10 22:13:13 +0200

    Login on client side

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,18 @@
-import { observable, action, computed, configure, runInAction } from "mobx";
+import { observable, action, computed, runInAction } from "mobx";
 import { IActivity } from "../models/activity";
-import { createContext, SyntheticEvent } from "react";
+import { SyntheticEvent } from "react";
 import agent from "../api/agent";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { RootStore } from "./rootStore";
 
-configure({ enforceActions: "always" });
+export default class ActivityStore {
+  rootStore: RootStore;
+  constructor(rootStore: RootStore){
+    this.rootStore = rootStore;
+  }
+  
 
-class ActivityStore {
   @observable activityRegistry = new Map();
   @observable loadingInitial: boolean = false;
   @observable activity: IActivity | null = null;
@@ -152,5 +157,3 @@ class ActivityStore {
     }
   };
 }
-
-export default createContext(new ActivityStore());
