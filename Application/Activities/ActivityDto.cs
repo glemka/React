127b2434447fc0214de2fc96250f0bd5637153f9
glemka <42246414+glemka@users.noreply.github.com>
2020-04-14 23:07:34 +0200

    make Guid public

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -6,7 +6,7 @@ namespace Application.Activities
 {
     public class ActivityDto
     {
-        Guid Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
