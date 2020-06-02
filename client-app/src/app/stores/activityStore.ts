ed3725307a3b2430ef79cbd102dec4e08f1a2b71
glemka <42246414+glemka@users.noreply.github.com>
2020-06-02 22:46:16 +0200

    Add filtering

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { observable, action, computed, runInAction } from 'mobx';
+import { observable, action, computed, runInAction, reaction } from 'mobx';
 import { IActivity } from '../models/activity';
 import { SyntheticEvent } from 'react';
 import agent from '../api/agent';
@@ -18,6 +18,14 @@ export default class ActivityStore {
   rootStore: RootStore;
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
+    reaction(
+      () => this.predicate.keys(),
+      () => {
+        this.page = 0;
+        this.activityRegistry.clear();
+        this.loadActivities();
+      }
+    );
   }
 
   @observable activityRegistry = new Map();
@@ -29,6 +37,29 @@ export default class ActivityStore {
   @observable.ref hubConnection: HubConnection | null = null;
   @observable activityCount: number = 0;
   @observable page = 0;
+  @observable predicate = new Map();
+
+  @action setPredicate = (predicate: string, value: string | Date) => {
+    this.predicate.clear();
+    debugger;
+    if (predicate !== 'all') {
+      this.predicate.set(predicate, value);
+    }
+  };
+
+  @computed get axiosParams() {
+    const params = new URLSearchParams();
+    params.append('limit', String(LIMIT));
+    params.append('offset', `${this.page ? this.page * LIMIT : 0}`);
+    this.predicate.forEach((value, key) => {
+      if (key === 'startDate') {
+        params.append(key, value.toISOString());
+      } else {
+        params.append(key, value);
+      }
+    });
+    return params;
+  }
 
   @computed get totalPages() {
     return Math.ceil(this.activityCount / LIMIT);
@@ -37,6 +68,7 @@ export default class ActivityStore {
   @action setPage = (page: number) => {
     this.page = page;
   };
+
   @computed get activitiesByDate() {
     return this.groupActivitiesByDate(
       Array.from(this.activityRegistry.values())
@@ -103,8 +135,8 @@ export default class ActivityStore {
   loadActivities = async () => {
     this.loadingInitial = true;
     try {
-      const activitiesEnvelope = await agent.Activities.list(LIMIT, this.page);
-      const {activities, activityCount} = activitiesEnvelope;
+      const activitiesEnvelope = await agent.Activities.list(this.axiosParams);
+      const { activities, activityCount } = activitiesEnvelope;
       runInAction('loading activities', () => {
         this.activityCount = activityCount;
         activities.forEach((act) => {
